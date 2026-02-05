0350c465a673392fc3bd1bf3de54ee368281586b
Houssam <mohamedhoussam211@gmail.com>
2026-02-05 11:01:50 +0100

    Fix lint and add Phase 2 completion walkthrough

--- lib/features/player/services/audio_player_service.dart
+++ lib/features/player/services/audio_player_service.dart
@@ -1,6 +1,5 @@
 import 'package:audio_service/audio_service.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:just_audio/just_audio.dart';
 import '../../../core/data/models/song.dart';
 import 'audio_handler.dart';
 
